318825233b53d9a7bb7fede66a5c13f0d78ffe68
Nguyen van luu <91738843+nguyendaithien@users.noreply.github.com>
2024-04-20 17:02:39 +0700

    Add files via upload

---
+++ CNN_accelator_5x5/5x5_4_output/src/WGT_BUFF.v
@@ -0,0 +1,65 @@
+module WGT_BUF 
+(
+	clk
+ ,rst_n
+ ,wgt_input
+ ,wgt_read
+ ,wgt_buf0
+ ,wgt_buf1
+ ,wgt_buf2
+ ,wgt_buf3
+ ,wgt_buf4
+);
+
+input clk;
+input rst_n;
+input signed [7:0] wgt_input;
+input wgt_read;
+output signed [7:0] wgt_buf0;
+output signed [7:0] wgt_buf1;
+output signed [7:0] wgt_buf2;
+output signed [7:0] wgt_buf3;
+output signed [7:0] wgt_buf4;
+
+reg signed [7:0] wgt_buf [4:0];
+
+
+
+integer i;
+
+always @(posedge clk or negedge rst_n) 
+    if (~rst_n) 
+    begin
+        for(i = 0; i < 5; i = i + 1)  
+        begin
+            wgt_buf[i] <= 0;
+        end
+    end 
+    else
+    begin
+        if(wgt_read)
+        begin
+            wgt_buf[4] <= wgt_buf[3];
+            wgt_buf[3] <= wgt_buf[2];
+            wgt_buf[2] <= wgt_buf[1];
+            wgt_buf[1] <= wgt_buf[0];
+            wgt_buf[0] <= wgt_input;
+        end
+        else
+        begin
+            wgt_buf[4] <= wgt_buf[4];
+            wgt_buf[3] <= wgt_buf[3];
+            wgt_buf[2] <= wgt_buf[2];
+            wgt_buf[1] <= wgt_buf[1];
+            wgt_buf[0] <= wgt_buf[0];
+        end
+    end 
+
+    assign wgt_buf0 = wgt_buf[0];
+    assign wgt_buf1 = wgt_buf[1];
+    assign wgt_buf2 = wgt_buf[2];
+    assign wgt_buf3 = wgt_buf[3];
+    assign wgt_buf4 = wgt_buf[4];
+
+endmodule 
+
